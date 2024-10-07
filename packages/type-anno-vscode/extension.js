7cc86df36be12858ca6fe9ebc8595e55268b1727
oneofthezombies <hunhoekim@gmail.com>
2024-10-07 21:49:31 +0900

    wip: vscode

--- packages/type-anno-vscode/extension.js
+++ packages/type-anno-vscode/extension.js
@@ -1,22 +1,75 @@
 const vscode = require("vscode");
+const { Project, SyntaxKind } = require("ts-morph");
+
+/** @type {Set<string>} */
+const filePathSet = new Set();
 
 /**
- * @param {vscode.ExtensionContext} context
+ * @param {vscode.ExtensionContext} extensionContext
  */
-function activate(context) {
+function activate(extensionContext) {
   import("@oneofthezombies/type-anno").then((typeAnno) => {
-    context.subscriptions.push(
+    extensionContext.subscriptions.push(
       vscode.languages.registerDocumentFormattingEditProvider("javascript", {
         provideDocumentFormattingEdits(document, options, token) {
-          vscode.window.showInformationMessage("called");
-          return undefined;
+          return new Promise((resolve) => {
+            (async () => {
+              const a = options;
+              const b = token;
+              const filePath = document.uri.fsPath;
+              try {
+                if (filePathSet.has(filePath)) {
+                  return;
+                }
+
+                filePathSet.add(filePath);
+
+                const project = new Project({
+                  useInMemoryFileSystem: true,
+                  compilerOptions: {
+                    allowJs: true,
+                    checkJs: false,
+                  },
+                });
+
+                const fileContent = document.getText();
+                const sourceFile = project.createSourceFile(
+                  filePath,
+                  fileContent,
+                  {
+                    overwrite: true,
+                  }
+                );
+
+                const edit = new vscode.WorkspaceEdit();
+                const fullRange = new vscode.Range(
+                  document.positionAt(0),
+                  document.positionAt(fileContent.length)
+                );
+                const transformedCode = sourceFile.getFullText();
+                edit.replace(document.uri, fullRange, transformedCode);
+                await vscode.workspace.applyEdit(edit);
+              } catch (error) {
+                vscode.window.showErrorMessage(error);
+              } finally {
+                filePathSet.delete(filePath);
+                resolve(null);
+              }
+            })();
+          });
         },
       })
     );
   });
 }
 
-function deactivate() {}
+/**
+ * aaa
+ * @param {number} a
+ * bbbb
+ * ccccc
+ */
+function deactivate(a) {}
 
 module.exports = {
   activate,
